86364612649da423a787990d90019a09bee58644
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-07 00:08:33 -0500

    Variables

---
+++ 3_variables.rb
@@ -0,0 +1,32 @@
+# Definir variables en Ruby es muy fácil:
+
+# el_nombre_de_mi_variable = "el valor que quiero"
+# puts el_nombre_de_mi_variable
+
+# En Ruby no hay que preocuparse por el
+# tipo de objeto. f*ck yeah!
+
+# name = 'Francesco'
+# lastname = "Rodriguez"
+# puts 'My fullname is ' + name + ' ' + lastname
+# puts "My fullname is #{name} #{lastname}"
+# age = 21
+# puts "I'm " + age.to_s
+# puts "I'm #{age}"
+# puts "I'm #{age - 5} ... ok, no u.u"
+
+# Como viste, las variables apuntan
+# a cualquier clase de objeto.
+
+# Si probamos:
+
+# username = "@frodsan"
+# username2 = username
+# puts username
+# puts username2
+
+# username = "@soyUnSuperTroll"
+# puts "Mi cuenta de twitter es #{username}"
+# puts "Mi cuenta de twitter es #{username2}"
+
+# Variables apuntan a objetos, no a otras variables :)
