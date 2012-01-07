fd97ae494b4d7a4501cf26ddbfe54b710898fbf4
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-06 23:48:29 -0500

    Numeros

---
+++ 1_numeros.rb
@@ -0,0 +1,48 @@
+# Ruby :D
+
+# puts: es la palabra mágica para mostrar
+# en pantalla cualquier cosa que viene
+# delante de ella.
+
+# Por ejemplo:
+
+# puts "Hola Mundo :)"
+
+# Recuerda: Para ejecutar código ruby 
+# descomenta la instrucción y en la
+# terminal escribe: 
+# ruby nombre_archivo
+
+# Como en cualquier lenguaje de programación,
+# ruby implementa números enteros como números
+# decimales:
+
+# Fixnum:
+# puts 0
+# puts 0.class
+
+# Float:
+# puts 0.0
+# puts 0.0.class
+
+# Operadores Básicos:
+# puts 7 + 7
+# puts 7.5 - 15
+# puts 3 * 2.0
+# puts 9 / 4
+
+# También puedes usar paréntesis
+# puts 10 * (2 * 3) + 1
+# puts 70 + ((10 - 1)/3) * -1
+
+
+# Ejercicio 1: ¿Cuántos minutos hay en un año?
+# RESPUESTA
+
+# Ejercicio 2: 
+# Convirtiendo enteros a decimales y viceversa
+# puts 10.to_f
+# puts 1.5.to_i
+
+# puts 1.5.ceil
+# puts 1.5.floor
