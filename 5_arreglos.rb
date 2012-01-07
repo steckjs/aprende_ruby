209b69c182bf9c26a28c5df91faddcaca1aac912
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-07 00:51:36 -0500

    arreglos

---
+++ 5_arreglos.rb
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+# ¿Cómo definir un arreglo?
+
+# array = []
+# puts array.class
+
+# array = Array.new
+# puts array.class
+
+# name = 'Francesco'
+# array = [11, name, 21.1589, ['ruby', 'javascript']]
+# puts array
+# puts array.inspect
+
+# ¿Cómo obtener valores?
+
+# array = [11, "hola", true]
+# puts array[0]
+# puts array[1]
+# puts array[2]
+# puts array[3]
+# puts array[3].inspect
+
+# El super método EACH
+
+# intereses = ["ruby", "javascript", "ember.js", "html5"]
+# intereses.each do |interes|
+#   puts "Adoro #{interes}"
+# end
+
+# What times is it?
+
+# 7.times do
+#   puts 'Debo visitar ruby.pe todos los días'
+# end
+
+# intereses = ["ruby", "javascript", "ember.js", "html5"]
+# puts intereses.length
+# puts intereses.first
+# puts intereses.last
+# intereses.push 'Haskell'
+# puts intereses.inspect
+# intereses.pop
+# puts intereses.inspect
+# intereses.shift
+# puts intereses.inspect
