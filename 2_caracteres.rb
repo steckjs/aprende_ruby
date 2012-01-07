ac329359aa9070156b684d60e90becff806e3e15
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-06 23:49:17 -0500

    Caracteres

---
+++ 2_caracteres.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+# Caracters? Cadenas de Caracteres? Palabras? Texto?
+
+# Es bastante simple, las cadenas se pueden
+# formar usando '' o "", asimismo pueden contenter
+# puntuación, símbolos, etc.
+
+# puts "Hola Ruby Peru!"
+# puts
+# puts 'Adios Java Cruel!'
+
+# También es posible concatenar cadenas usando
+# el operador +.
+
+# puts 'I ♥ ' + 'Ruby'
+# puts 'I ♥ Ruby ' * 5
+
+# Concatenar una cadena
+# con un número?
+
+# puts '12' + 12
+# puts '12' + 12.to_s
+# puts '12'.to_i + 12
+
+# Entre otras cosas:
+
+# puts "Esto generará un error? "Sí""
+# puts "Funciona? 'Sí, funciona'"
+# puts 'Funciona? "Sí, funciona"'
+# puts "Funciona? \"Sí, funciona\""
+# puts 'Funciona? \'Sí, funciona\''
